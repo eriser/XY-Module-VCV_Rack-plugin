f89dc12fab34ea1bf47d57a5b0ed404d6e1b7a0a
U-user-PC\user <abel@github.com>
2017-11-05 16:54:32 -0300

    first commit

--- src/Tutorial.cpp
+++ src/Tutorial.cpp
@@ -13,7 +13,8 @@ void init(rack::Plugin *p) {
 #endif
 
 	// For each module, specify the ModuleWidget subclass, manufacturer slug (for saving in patches), manufacturer human-readable name, module slug, and module name
-	p->addModel(createModel<MyModuleWidget>("Tutorial", "Tutorial", "MyModule", "My Module"));
+	p->addModel(createModel<AlacsonyW>("Tutorial", "Tutorial", "ALCS", "Alacsony"));
+	p->addModel(createModel<MagasW>("Tutorial", "Tutorial", "MGS", "Magas"));
 
 	// Any other plugin initialization may go here.
 	// As an alternative, consider lazy-loading assets and lookup tables when your module is created to reduce startup times of Rack.
